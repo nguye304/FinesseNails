9ee245822e186714d0ba8c8410bf049643dd3122
Kevin <kevin.nguyen1222@gmail.com>
2019-01-05 10:16:58 -0800

    Initial Commit w/Background Image

---
+++ FinesseNailsApp/FinesseNailsApp/AppointmentPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace FinesseNailsApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AppointmentPage : ContentPage
+	{
+		public AppointmentPage ()
+		{
+			InitializeComponent ();
+		}
+
+        async void OnRootClicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PopToRootAsync();
+        }
+    }
+}
---
+++ FinesseNailsApp/FinesseNailsApp/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FinesseNailsApp
+{
+
+
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        async void OnRootClicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PopToRootAsync();
+        }
+
+        void OnAppClicked(object sender, System.EventArgs e)
+        {
+           Navigation.PushAsync(new AppointmentPage());
+        }
+
+       void OnPriceClicked(object sender, System.EventArgs e)
+        {
+           Navigation.PushAsync(new PricePage());
+        }
+
+       void OnInfoClicked(object sender, System.EventArgs e)
+        {
+           Navigation.PushAsync(new InfoPage());
+        }
+    }
+}
