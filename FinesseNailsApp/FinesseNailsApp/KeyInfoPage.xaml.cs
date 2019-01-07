8de149e4e4b5234234e92e39abc196536b5df9be
Kevin <kevin.nguyen1222@gmail.com>
2019-01-07 12:54:56 -0800

    Implemented Grid and Navigation

---
+++ FinesseNailsApp/FinesseNailsApp/KeyInfoPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FinesseNailsApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class KeyInfoPage : ContentPage
+	{
+		public KeyInfoPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
--- FinesseNailsApp/FinesseNailsApp/MainPage.xaml.cs
+++ FinesseNailsApp/FinesseNailsApp/MainPage.xaml.cs
@@ -16,26 +16,49 @@ namespace FinesseNailsApp
         {
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
+
+            var grid = new Grid();
+
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+
+            var topLeft = new Label { Text = "Top Left" };
+            var topRight = new Label { Text = "Top Right" };
+            var bottomLeft = new Label { Text = "Bottom ANA IS HOT Left" };
+            var bottomRight = new Label { Text = "Bottom DURR RIGHT FKR Right" };
+
+            grid.Children.Add(topLeft, 0, 0);
+            grid.Children.Add(topRight, 1, 0);
+            grid.Children.Add(bottomLeft, 0, 1);
+            grid.Children.Add(bottomRight, 1, 1);
+
         }
 
         async void OnRootClicked(object sender, System.EventArgs e)
         {
-            await Navigation.PopToRootAsync();
+            await Navigation.PopToRootAsync();//go back to home
         }
 
         void OnAppClicked(object sender, System.EventArgs e)
         {
-           Navigation.PushAsync(new AppointmentPage());
+           Navigation.PushAsync(new AppointmentPage());//Make an Appointment Page
         }
 
-       void OnPriceClicked(object sender, System.EventArgs e)
+       void OnContactClicked(object sender, System.EventArgs e)
         {
-           Navigation.PushAsync(new PricePage());
+           Navigation.PushAsync(new PricePage());//PricePage
         }
 
        void OnInfoClicked(object sender, System.EventArgs e)
         {
-           Navigation.PushAsync(new InfoPage());
+           Navigation.PushAsync(new InfoPage());//EmployeeInfoPage
+        }
+
+        private void OnKeyInfoClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new KeyInfoPage());//Price,Location,Hours Page
         }
     }
 }
