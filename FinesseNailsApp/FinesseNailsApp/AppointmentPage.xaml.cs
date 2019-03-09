46fef4970c5ecc39736f01e6a2c2a5bac67b2a9a
Kevin <kevin.nguyen1222@gmail.com>
2019-03-09 15:04:16 -0800

    Implemented a way to choose Date and Time for appointment.

--- FinesseNailsApp/FinesseNailsApp/AppointmentPage.xaml.cs
+++ FinesseNailsApp/FinesseNailsApp/AppointmentPage.xaml.cs
@@ -22,5 +22,26 @@ namespace FinesseNailsApp
         {
             await Navigation.PopToRootAsync();
         }
+
+        DatePicker datePicker = new DatePicker
+        {
+            MinimumDate = new DateTime(2019,1,1),
+            MaximumDate = new DateTime(2019,12,31),
+            Date = new DateTime(2019,7,12),
+            
+
+        };
+        TimePicker timePicker = new TimePicker
+        {
+            Time = new TimeSpan(4,15,26)//set to 04:14:26 so hour, mi
+        };
+
+        private void OnDateSelected(object sender, DateChangedEventArgs e)
+        {
+            DisplayAlert("Selected Date is: ", "TIME IS HERE ", "OK");
+        }
+        //TODO: 
+
+
     }
 }
